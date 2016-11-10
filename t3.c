deba25fefe57d7c2529e967226880098c1c7e0b8
LucidThought <alata.it@gmail.com>
2016-11-10 15:27:56 -0700

    Win conditions completed. X wins

--- t3.c
+++ t3.c
@@ -344,6 +344,50 @@ char testWin()
 			return 'O'; // If horizontal line - X wins
 		}
 	}
+	else if(board[0][0]=='X')
+	{
+		if(board[0][1]=='X' && board[0][2]=='X')
+		{
+			return 'X';
+		}
+		else if(board[1][0]=='X' && board[2][0]=='X')
+		{
+			return 'X';
+		}
+	}
+	else if(board[0][0]=='O')
+	{
+		if(board[0][1]=='O' && board[0][2]=='O')
+		{
+			return 'O';
+		}
+		else if(board[1][0]=='O' && board[2][0]=='O')
+		{
+			return 'O';
+		}
+	}
+	else if(board[2][2]=='X')
+	{
+		if(board[2][0]=='X' && board[2][1]=='X')
+		{
+			return 'X';
+		}
+		else if(board[1][2]=='X' && board[0][2]=='X')
+		{
+			return 'X';
+		}
+	}
+	else if(board[2][2]=='O')
+	{
+		if(board[2][0]=='O' && board[2][1]=='O')
+		{
+			return 'O';
+		}
+		else if(board[1][2]=='O' && board[0][2]=='O')
+		{
+			return 'O';
+		}
+	}
 
 	return 'Z';
 }
