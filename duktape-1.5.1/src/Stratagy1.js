61574f87ea096ac211bf16f3e3715c008892d1b9
LauraBerry <lauraberry963@hotmail.ca>
2016-11-11 16:06:36 -0700

    new stratagies

--- duktape-1.5.1/src/Stratagy1.js
+++ duktape-1.5.1/src/Stratagy1.js
@@ -1,38 +1,10 @@
-function freed_space(board, b, c)
+function stratagy1()
 {
-	if(a[b][c]=='0')
+	a=Math.random();
+	a=a*20;
+	while(a>2)
 	{
-		return true;
+		a=a/2;
 	}
-	else
-	{
-		return false;
-	}
-}
-
-function Stratagy1(board)
-{
-	turn=false;
-	a=0;
-	b=0;
-	while (!turn)
-	{
-		turn=freed_space(board,a,b);
-		if(!turn)
-		{
-			if (a<3)
-			{
-				a++;
-			}
-			else
-			{
-				a=0;
-				b++;
-			}
-		}
-	}
-	result = new int[2];
-	result[0]=a;
-	result[1]=b;
-	return result;
+	return a;
 }
--- duktape-1.5.1/src/Stratagy2.js
+++ duktape-1.5.1/src/Stratagy2.js
@@ -1,69 +1,22 @@
-function diagonal_possible(char [][] a)
+function stratagy2(a,b)
 {
-	if(a[0][0]=='x' && a[2][2]=='x')
+	var indexes = new Array();
+	if (a<2)
 	{
-		return true;
-	}
-	else if (a[0][2]=='x' && a[2][0]==='x')
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-function boolean is_it_clear(char [][]a, int b, int c)
-{
-	if(a[b][c]==' ')
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-function int[] Stratagy2(char[][] board)
-{
-	boolean diagonal=false;
-	boolean clear=false;
-	boolean all_corners_tried=false;
-	int a;
-	int b;
-	while(!clear)
-	{
-		diagonal=diagonal_possible(board);
-		if (diagonal==true)
+		if(b>=2)
 		{
-			a=1;
-			b=1;
-			clear=is_it_clear(board,a,b);
-		}
-		else if (all_corners_tried==false)
-		{
-			a=0;
 			b=0;
-			clear=is_it_clear(board,a,b);
-			a=2;
-			clear=is_it_clear(board,a,b);
-			b=2;
-			clear=is_it_clear(board,a,b);
-			a=0;
-			clear=is_it_clear(board,a,b);
-			all_corners_tried=false;
-		}
-		else
-		{
-			if(b<3)
-			{
-				b++;
-			}
-			else
-			{
-				b=0;
-				a++;
-			}
 		}
 	}
+	else
+	{
+		a=0;
+		if(b>=2)
+		{
+			b=0;
+		}
+	}
+	indexes[0]=a;
+	indexes[1]=b;
+	return indexes;
 }
