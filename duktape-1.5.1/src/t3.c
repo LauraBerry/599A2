9d44909ab58e14e612c182f060d37ebcd3e74b55
LucidThought <alata.it@gmail.com>
2016-11-14 21:00:40 -0700

    added print statement to show the game board string as it is changed on each turn

--- duktape-1.5.1/src/t3.c
+++ duktape-1.5.1/src/t3.c
@@ -536,6 +536,7 @@ int main(int argc, const char *argv[])
 				}
 			}
 			tempString[tempInt] = '\0';
+			mvprintw(18,6,tempString);
 //			boardtostring(tempstring);
 			duk_push_string(ctx, tempString);
 			if(duk_pcall(ctx,1) != 0)
